d9d6c272fa1454ab6e873203a32be8a1b674bbc3
Marcos Esteban Gomez <marcosgomez@cs.arizona.edu>
2015-03-28 18:53:34 -0700

    worked on sr_init

--- Project2/top317/sr_router.c
+++ Project2/top317/sr_router.c
@@ -45,14 +45,20 @@ int ioctl_sock;
 void sr_init(struct sr_instance* sr)
 {
     struct in_addr my_in_addr;
-    u_char hwaddr[ETHER_ADDR_LEN];
-    uint8_t *buf = NULL; //1 byte at a time
+    unsigned char hwaddr[ETHER_ADDR_LEN];
+    void *buf = NULL; //1 byte at a time uint8_t
     unsigned int len = 0; //Later use sizeof to finde how many bytes
-    const char* iface = "eth1";
+    const char* iface = "eth1"; //eth1 doesnt exist // prob with eth header
+    char* sender = ""; //"" = myself
 
     struct sr_arphdr arphdr;
 
-    ioctl_sock = socket(AF_INET, SOCK_PACKET, htons(ETH_P_RARP));
+    //ioctl_sock = socket(AF_INET, SOCK_PACKET, htons(ETH_P_RARP));
+    //ioctl_sock = socket(SOL_SOCKET, SOCK_RAW, ETHERTYPE_REVARP);
+    ioctl_sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
+    if(ioctl_sock < 0){
+        fprintf(stderr, "sr_init: Unable to create socket\n");
+    }
 
 
     /* REQUIRES */
@@ -60,8 +66,8 @@ void sr_init(struct sr_instance* sr)
 
     /* Add initialization code here! TODO*/
 
-    get_hw_addr(hwaddr, iface);
-    get_ip_addr(&my_in_addr, iface);
+    get_hw_addr(hwaddr, sender);
+    get_ip_addr(&my_in_addr, sender);
 
     //Send out ARP request to all servers on LAN to build table
     //Create ARP request packet
@@ -75,19 +81,31 @@ void sr_init(struct sr_instance* sr)
     //Type
     arphdr.ar_op = htons(ARP_REQUEST);
     //Sender/Target IP/Hardware addresses
-    // arphdr.ar_sha
+    
+    for(int i = 0; i < ETHER_ADDR_LEN; i++){
+        arphdr.ar_sha[i] = hwaddr[i];
+    }
 
-    // arphdr.ar_sip
-
-    // arphdr.ar_tha
-
-    // arphdr.ar_tip
+    
+    for(int i = 0; i < ETHER_ADDR_LEN; i++){
+        arphdr.ar_tha[i] = 0xff; //Broadcast MAC addresses ff:ff:ff:ff:ff:ff
+    }
+    
+    memcpy(&arphdr.ar_sip, &my_in_addr, IP_ADDR_LEN);
+    memcpy(&arphdr.ar_tip, &my_in_addr, IP_ADDR_LEN);
+    //Debug("sr_int: memcpyed IP address: %s\n", inet_ntoa((struct in_addr)arphdr.ar_sip));
+     
 
     //Find out which interface to send them
     iface = "eth1";
+    //Set buffer
+    buf = &arphdr;
+    len = sizeof(arphdr);
+    Debug("sr_init: len = %u\n", len);
     
     //Send packets
     sr_send_packet(sr, buf, len, iface); //sr_vns_comm.c
+    Debug("Successfully completed sr_init\n");
 
 } /* -- sr_init -- */
 
@@ -143,11 +161,45 @@ void get_ip_addr(struct in_addr* in_addr,char* str)
 {
     struct ifreq ifr;
     struct sockaddr_in sin;
-
-    strcpy(ifr.ifr_name, str);
-    ifr.ifr_addr.sa_family = AF_INET;
-    if (ioctl(ioctl_sock, SIOCGIFADDR, &ifr))
-        die("Failed to get IP address for the interface");
+    
+    if(strcmp(str, "") == 0){
+        Debug("Using own IP address\n");
+        struct ifconf ifc;
+        char buffer[1024];
+        
+        
+        ifc.ifc_len = sizeof(buffer);
+        ifc.ifc_buf = buffer;
+        if(ioctl(ioctl_sock, SIOCGIFCONF, &ifc) == -1){
+            fprintf(stderr, "get_ip_addr: socket error\n");
+        }
+        
+        struct ifreq* it = ifc.ifc_req;
+        const struct ifreq* const end = it + (ifc.ifc_len /sizeof(struct ifreq));
+        
+        for(; it != end; ++it){
+            strcpy(ifr.ifr_name, it->ifr_name);
+            ifr.ifr_addr.sa_family = it->ifr_addr.sa_family;
+            //ifr.ifr_addr.sa_family = AF_INET;
+            if(ioctl(ioctl_sock, SIOCGIFFLAGS, &ifr) == 0){
+                if(!(ifr.ifr_flags & IFF_LOOPBACK)){//don't count loopback
+                    if(ioctl(ioctl_sock, SIOCGIFADDR, &ifr) == 0){
+                        //Success
+                        break;
+                    }else{
+                        die("Failed to get IP address for the interface");
+                    }
+                }
+            }else{
+                fprintf(stderr, "get_ip_addr: Error inside loop with flags\n");
+            }
+        }
+    }else{
+        strcpy(ifr.ifr_name, str);
+        ifr.ifr_addr.sa_family = AF_INET;
+        if (ioctl(ioctl_sock, SIOCGIFADDR, &ifr))
+            die("Failed to get IP address for the interface");
+    }
 
 
     memcpy(&sin, &ifr.ifr_addr, sizeof(struct sockaddr_in));
@@ -155,17 +207,51 @@ void get_ip_addr(struct in_addr* in_addr,char* str)
     Debug("IP address: %s\n", inet_ntoa(*in_addr));
 }
 
+
+
 void get_hw_addr(u_char* buf,char* str)
 {
-    //int i;
+  
     struct ifreq ifr;
-    //char c,val = 0;
+    
+    if(strcmp(str, "") == 0){
+        Debug("Using own hardware address\n");
+        struct ifconf ifc;
+        char buffer[1024];
+        
+        
+        ifc.ifc_len = sizeof(buffer);
+        ifc.ifc_buf = buffer;
+        if(ioctl(ioctl_sock, SIOCGIFCONF, &ifc) == -1){
+            fprintf(stderr, "get_hw_addr: socket error\n");
+        }
+        
+        struct ifreq* it = ifc.ifc_req;
+        const struct ifreq* const end = it + (ifc.ifc_len /sizeof(struct ifreq));
+        
+        for(; it != end; ++it){
+            strcpy(ifr.ifr_name, it->ifr_name);
+            if(ioctl(ioctl_sock, SIOCGIFFLAGS, &ifr) == 0){
+                if(!(ifr.ifr_flags & IFF_LOOPBACK)){//don't count loopback
+                    if(ioctl(ioctl_sock, SIOCGIFHWADDR, &ifr) == 0){
+                        //Success
+                        break;
+                    }else{
+                        die("Failed to get own MAC address for the interface");
+                    }
+                }
+            }else{
+                fprintf(stderr, "get_hw_addr: Error inside loop with flags\n");
+            }
+        }
+    }else{
+        strcpy(ifr.ifr_name, str);
+        if (ioctl(ioctl_sock, SIOCGIFHWADDR, &ifr) < 0)
+            die("Failed to get MAC address for the interface");
+    }
+    
 
-    strcpy(ifr.ifr_name, str);
-    if (ioctl(ioctl_sock, SIOCGIFHWADDR, &ifr))
-        die("Failed to get MAC address for the interface");
-
-    memcpy(buf, ifr.ifr_hwaddr.sa_data, 8);
+    memcpy(buf, ifr.ifr_hwaddr.sa_data, ETHER_ADDR_LEN);
     Debug("MAC address: %02X:%02X:%02X:%02X:%02X:%02X\n", *(buf), *(buf+1), *(buf+2), *(buf+3),*(buf+4), *(buf+5));
 }
 
